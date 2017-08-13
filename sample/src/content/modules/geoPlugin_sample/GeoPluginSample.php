c81ba395fe7b4cffe627661e5fc02408619a54d7
Ulrich Schmidt <admin@deruli.de>
2017-08-13 12:21:36 +0200

    case fehler korrigiert

--- sample/src/content/modules/geoPlugin_sample/GeoPluginSample.php
+++ sample/src/content/modules/geoPlugin_sample/GeoPluginSample.php
@@ -6,7 +6,7 @@ class GeoPluginSample extends Controller
     public function render()
     {
         ob_start();
-        $controller = ModuleHelper::getMainController("GeoPlugin");
+        $controller = ModuleHelper::getMainController("geoPlugin");
         var_dump($controller->getGeoLocation());
         return ob_get_clean();
     }

0a728a26b40b0cb1607579b638b97e8b106c1891
Ulrich Schmidt <admin@deruli.de>
2017-08-17 19:41:33 +0200

    geoPlugin 1.1 nutze json statt PHP-Serialisierung wegen Performance

--- src/content/modules/geoPlugin/GeoPlugin.php
+++ src/content/modules/geoPlugin/GeoPlugin.php
@@ -1,9 +1,14 @@
 <?php
-class GeoPlugin extends Controller {
-	public function getGeoLocation($ip = null) {
-		if (is_null ( $ip )) {
-			$ip = get_ip ();
-		}
-		return unserialize ( file_get_contents_wrapper ( 'http://www.geoplugin.net/php.gp?ip=' . $ip, true ) );
-	}
+
+class GeoPlugin extends Controller
+{
+
+    public function getGeoLocation($ip = null)
+    {
+        if (is_null($ip)) {
+            $ip = get_ip();
+        }
+        
+        return json_decode(file_get_contents_wrapper('http://www.geoplugin.net/json.gp?ip=' . $ip, true), true);
+    }
 }
